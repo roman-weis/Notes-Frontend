60c84bbc400be878cc54cbb5ef6b9ea2ba86087f
Roman Weis <rweis@axioconcept.com>
2021-05-03 15:11:14 +0200

    card and container added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
+import { NotesListComponent } from './pages/notes-list/notes-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:MainLayoutComponent, children:[
+{path:'',component: NotesListComponent}]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
